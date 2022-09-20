e767e41046db0b74250d06d15dab2e256e83588a
sachieko <arashikabane@gmail.com>
2022-09-20 12:27:11 -0600

    update main

---
+++ main.rb
@@ -0,0 +1,25 @@
+=begin
+
+Player class: -Could be bound up in the game class as two player properties but what if players had custom display names?
+name: (init one with player 1 and one with player 2)
+health: init with 3 life read
+health_total: init with 3 life read
+subtract 1 from health method
+
+Game class:
+Run the game method
+Track current player: Init with player 1
+change current player method - attr_accessor since it must also output current player to screen as well as change
+could set a time limit-
+winner
+
+Question class
+allows a user to input answer to questions
+method to randomly generate an integer between 1 and 20
+a: integer between 1 and 20 - randomly generated each time
+b: integer between 1 and 20 - see above
+question: "What does #{a} + #{b} equal?" output and ask for input from user
+answer: what the current player put as the answer
+
+
+=end
